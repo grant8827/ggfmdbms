781ba2767c62d4992b74cce84f4600e1ba66bbc2
Gregory Grant <grant8827@yahoo.com>
2025-07-02 13:22:10 -0400

    test hosting

--- ggfm_dbs_project/settings.py
+++ ggfm_dbs_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+pz7w@8zr03a23=his&q-*6o)jps#xwj67zgcla^-o!mhd=f=h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['ggfmdbms.vercel.app' 'www.ggfmdbms.vercel.app/',  ] # For local development
+ALLOWED_HOSTS = [] # For local development
 # When deploying, you'll add your actual domain names and server IP here
 #ALLOWED_HOSTS = ['your_domain.com', 'www.your_domain.com', 'your_server_ip_address', '127.0.0.1', 'localhost']
 
